5cd841449aef93db64b9e45a145ad800599f653e
鲸落 <1656855152@qq.com>
2024-09-07 11:22:59 +0800

    项目初始化

---
+++ heima-leadnews-common/src/main/java/com/htx/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.htx.common.exception;
+
+import com.htx.model.common.enums.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}
